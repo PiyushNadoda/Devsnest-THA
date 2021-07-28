4aab4dca70d1117b3fe353795ae28d6346a2be3d
BLiTz <69067256+PiyushNadoda@users.noreply.github.com>
2021-07-28 14:31:49 +0530

    Day 9 redesigned

--- Day09/script.js
+++ Day09/script.js
@@ -1,20 +1,22 @@
-let totalseats = 25
-let seats = 0
-
-for (let index = 0; index < totalseats; index++) {
-    const seatelement = document.createElement("div");
-    seatelement.innerHTML = `<p class="button">🪑 : ${index} open</p>`;
-    seatelement.setAttribute('id',index);
-    seatelement.setAttribute('class',"seat");
-    document.getElementById('seats').insertAdjacentElement('beforeend',seatelement);
-};
-
-Array.from(document.querySelectorAll(".seat")).forEach((element,index) => {
-    element.addEventListener("click", (x) => {
-        let result = element.children[0].classList.toggle('is-warning');
-        if (result) {seats++ ; element.children[0].innerHTML='booked 😊' ;}
-        else {seats-- ; element.children[0].innerHTML= `🪑 : ${element.id} open`;}
-        document.querySelector("#booked").innerHTML = "Booked Seats : " + seats;
-        const remaining = document.querySelector("#remaining").innerHTML =  `Remaining Seats : ${totalseats - seats}`;
-    }); 
-});
+var bookedSeats = document.querySelector('.booked-seats')
+var reaminingSeats = document.querySelector('.reamining-seats')
+var count = 0
+window.onclick = e => {
+    if(
+        e.target.classList.contains('learn-more')
+    ){
+        if(e.target.classList.contains('booked')){
+            e.target.classList.remove('booked')
+            count--;
+            bookedSeats.innerText = `Booked seats : ${count}`
+            reaminingSeats.innerText = `Remaining seats : ${20 - count}`  
+        }
+        else{
+            e.target.classList.add('booked')
+            count++;
+            bookedSeats.innerText = `Booked seats ${count}`
+            reaminingSeats.innerText = `Remaining seats ${20 - count}`  
+        }
+    }
+    }
+   
